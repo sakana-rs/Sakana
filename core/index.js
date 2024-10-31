0de7e5953a3cb7d64e90c28b6bdd6bb31adc32d9
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-10-31 21:23:32 +0100

    Update index.js

--- core/index.js
+++ core/index.js
@@ -4,8 +4,9 @@ import Runner from './run.js'
 export class Sakana {
   constructor(id) {
     this.canvas = document.getElementById(id);
+    this.game = null;
   }
   load(file) {
-    Runner.run(Decoder(file))
+    Runner.run(Decoder(file), this)
   }
 }

99611bd94e3dd52a8694aa7e070a9eb02de85692
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-10-31 23:15:43 +0100

    Update index.js

--- core/index.js
+++ core/index.js
@@ -3,13 +3,15 @@ import { Runner } from './run.js'
 export class Sakana {
   constructor(id) {
     console.log('[Sakana] Starting Sakana')
+    this.root = document.getElementById(id);
     // Game canvas
-    const canvas = document.getElementById(id);
+    const canvas = document.createElement('canvas');
     this.canvas = canvas;
     this.width = 1280;
     this.height = 720;
     canvas.width = 1280;
     canvas.height = 720;
+    this.root.appendChild(canvas);
 
     this.ctx = canvas.getContext('2d');
     this.game = null;

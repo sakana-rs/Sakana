0480b4975b01d592c5c47e5ac3c467f0ec793daa
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-10-31 23:17:45 +0100

    Update script.js

--- web/script.js
+++ web/script.js
@@ -5,3 +5,13 @@ let emulator = new Sakana('game');
 document.getElementById('file').onchange = function(evt){
   emulator.load(evt.target.files[0])
 }
+
+document.getElementById('b-p').onclick = function(){
+  emulator.game.pause()
+}
+document.getElementById('b-r').onclick = function(){
+  emulator.game.resume()
+}
+document.getElementById('b-f').onclick = function(){
+  emulator.game.fullscreen()
+}

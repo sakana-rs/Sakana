ee8b04803e1c3d1e6ad6ee54deb273d1b531a2fa
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-10-31 21:19:25 +0100

    Update index.js

--- core/index.js
+++ core/index.js
@@ -1,10 +1,11 @@
-class Sakana {
+import Decoder from './decode.js'
+import Runner from './run.js'
+
+export class Sakana {
   constructor(id) {
     this.canvas = document.getElementById(id);
   }
-  load() {
-    // Code :3
+  load(file) {
+    Runner.run(Decoder(file))
   }
 }
-
-export Sakana

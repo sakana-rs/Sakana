2a4fe1f1e6e2dfa49e1fd56abfc10edafda30846
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-10-31 23:02:06 +0100

    Update run.js

--- core/run.js
+++ core/run.js
@@ -10,21 +10,27 @@ export class Runner {
     // Blank out the screen
     this.Sakana.ctx.fillStyle = 'black';
     this.Sakana.ctx.fillRect(0, 0, this.Sakana.width, this.Sakana.height);
-    // Write text
+    // Write name
     this.Sakana.ctx.fillStyle = 'white';
     this.Sakana.ctx.font = '48px Arial';
     this.Sakana.ctx.textAlign = 'center';
     this.Sakana.ctx.textBaseline = 'middle';
-    this.Sakana.ctx.fillText(text, this.Sakana.width / 2, this.Sakana.height / 2);
+    this.Sakana.ctx.fillText('Sakana', this.Sakana.width / 2, this.Sakana.height / 2 - 24);
+    // Write text
+    this.Sakana.ctx.fillStyle = 'white';
+    this.Sakana.ctx.font = '24px Arial';
+    this.Sakana.ctx.textAlign = 'center';
+    this.Sakana.ctx.textBaseline = 'middle';
+    this.Sakana.ctx.fillText(text, this.Sakana.width / 2, this.Sakana.height / 2 + 24);
   }
   run(code) {
     const mspf = 1000 / this.framerate; // Convert from FPS to MsPF (Miliseconds per Frame)
     const _this = this;
     setInterval(function(){
       if (_this.loading) {
-        _this.drawTextScreen('Sakana\nLoading...');
+        _this.drawTextScreen('Loading...');
       } else if (this.paused) {
-        _this.drawTextScreen('Sakana\nPaused');
+        _this.drawTextScreen('Paused');
       } else {
         // game
       }

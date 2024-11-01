e57c79b9e08145f1f0c7a203eb120e9de2f621ae
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-11-01 22:12:20 +0100

    Helpful change

--- web/script.js
+++ web/script.js
@@ -1,6 +1,7 @@
 import { Sakana } from '../core/index.js'
 
 let emulator = new Sakana('game');
+window.emulator = emulator;
 
 document.getElementById('run').onclick = function(){
   emulator.load(document.getElementById('file').files[0], document.getElementById('keys').files[0])

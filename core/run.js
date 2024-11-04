c0ed6c26cf4e6e76e23ca2c876e09596c60d1860
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-11-04 22:57:52 +0100

    Logo

--- core/run.js
+++ core/run.js
@@ -19,6 +19,11 @@ export class Runner {
     this.Sakana.ctx.textAlign = 'center';
     this.Sakana.ctx.textBaseline = 'middle';
     this.Sakana.ctx.fillText('Sakana', this.Sakana.width / 2, this.Sakana.height / 2 - 24);
+    let logo = new Image();
+    logo.onload = function() {
+      ctx.drawImage(logo, this.Sakana.width / 2 - 30, this.Sakana.height / 2 - 24);
+    }
+    logo.src = "./core/sakana.svg";
     // Write text
     this.Sakana.ctx.fillStyle = 'white';
     this.Sakana.ctx.font = '24px Arial';

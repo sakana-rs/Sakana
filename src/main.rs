94ac9e1b707f1ca59d8564cb879b22a88ead5a48
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-03-07 22:36:19 +0100

    Remove old | New main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
+use std::fs::File;
 use cntx;
 
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", File::open("test.nsp").unwrap());
+    let pfs0_reader = cntx::util::new_shared(File::open("test.nsp").unwrap());
+    let pfs0 = cntx::pfs0::PFS0::new(pfs0_reader).unwrap();
+
+    let files = pfs0.list_files().unwrap();
+    println!("Files: {:?}", files);
 }

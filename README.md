66a3e6a2fdbfdc65e15af55427225c3a20b5fbb7
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-03-15 16:41:24 +0100

    Nca

--- README.md
+++ README.md
@@ -8,19 +8,3 @@ We still are making sakana, please be patient, find our roadmap at [#1](https://
 # Sakana
 Web based Switch emulator.\
 Sakana implements the core for Switch game emulation.
-
-## Add it to your website!
-Using Sakana is simple, just import the /core/index.js file and call the emulator.
-```js
-import { Sakana } from 'path/to/sakana/core/index.js'
-
-let emulator = new Sakana('div id');
-
-// Sakana is made to work directly with files from inputs
-emulator.load(game file, key file);
-```
-And it has a easy to use api.
-```js
-emulator.game.pause();
-```
-Find more info in the [wiki](https://sakana-rs.github.io/Docs/sakana/api).
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
 use std::fs::File;
+use std::env;
+use std::io::Cursor;
 use cntx;
 
 fn main() {
-    println!("{:?}", File::open("test.nsp").unwrap());
-    let pfs0_reader = cntx::util::new_shared(File::open("test.nsp").unwrap());
-    let pfs0 = cntx::pfs0::PFS0::new(pfs0_reader).unwrap();
+    let args: Vec<String> = env::args().collect();
+
+    let keyset = cntx::key::Keyset::from(File::open(&args[2]).unwrap()).unwrap();
+    let pfs0_reader = cntx::util::new_shared(File::open(&args[1]).unwrap());
+    let mut pfs0 = cntx::pfs0::PFS0::new(pfs0_reader).unwrap();
 
     let files = pfs0.list_files().unwrap();
     println!("Files: {:?}", files);
+
+    let mut idx: usize = 0;
+    for file in files.iter() {
+        println!("File: {}", file);
+
+        if file.ends_with(".nca") {
+            let size = pfs0.get_file_size(idx).unwrap();
+            let mut file_buf = vec![0u8; size];
+            pfs0.read_file(idx, 0, &mut file_buf).unwrap();
+
+            let nca_reader = cntx::util::new_shared(Cursor::new(file_buf));
+            let mut nca = cntx::nca::NCA::new(nca_reader, &keyset, None).unwrap();
+        }
+
+        idx += 1;
+    }
 }
